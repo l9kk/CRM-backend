09fb935a3e7cc9146fe9ab3b7edcda5a15ef0110
l9kk <askhat.ss23@gmail.com>
2025-01-10 22:32:51 +0500

    "fixed email response to projects"

--- crm/settings.py
+++ crm/settings.py
@@ -116,6 +116,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'crm.wsgi.application'
 
 
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
